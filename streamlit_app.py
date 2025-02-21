0fc4ef4fea0c49d91e2c48b39ffb731ac4b1ec26
Youjin Song <ujinsong23@gmail.com>
2025-02-20 21:25:57 -0800

    compare more than 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,19 +2,29 @@ import streamlit as st
 import os
 
 OPTIONS = [
-        {
-            "3s-m1-4000step": "videos/3sec/m1_newtest/step-4000",
-            "3s-mamba-4000step": "videos/3sec/mamba2_newtest/step-4000" 
-        },
+        # {
+        #     "3s-m1-4000step": "videos/3sec/m1_newtest/step-4000",
+        #     "3s-mamba-4000step": "videos/3sec/mamba2_newtest/step-4000" 
+        # },
         {
             "3s-m2-2500step": "videos/3sec/m2_newtest/step-2500",
             "3s-mamba-2500step": "videos/3sec/mamba2_newtest/step-2500",
+        },
+        {
+            "3s-m1-4000step": "videos/3sec/m1_newtest/step-4000",
+            "3s-m2-4000step": "videos/3sec/m2_newtest/step-4000",
+            "3s-mamba-4000step": "videos/3sec/mamba2_newtest/step-4000",
         }
     ]
 
+def simplify_model_names(model_names):
+    if len(model_names) < 3:
+        return model_names
+    else:
+        return [model_name.split("-")[1] for model_name in model_names]
+
 def show_selected_comparison(upload_folder_dict):
-    model_names = list(upload_folder_dict.keys())
-    st.header(" vs. ".join(model_names))
+    st.caption(f"negative_prompt=motion blur, distorted faces, abnormal eyes")
 
     for video_id in range(16):
         st.subheader(f"Sample #{video_id}")
@@ -35,7 +45,7 @@ if __name__ == "__main__":
     """
 
     OPTIONS = {
-        " vs. ".join(list(upload_folder_dict.keys())):upload_folder_dict for upload_folder_dict in OPTIONS
+        " vs. ".join(simplify_model_names(upload_folder_dict.keys())):upload_folder_dict for upload_folder_dict in OPTIONS
     }
 
     option = st.selectbox(
