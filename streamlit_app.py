c3796aa15554fcaabca6a470c84c40dc9daa0e83
Youjin Song <ujinsong23@gmail.com>
2025-02-22 20:29:46 -0800

    added 8k for all 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,20 +13,29 @@ OPTIONS = [
             "video_length": 9,
             "neg_prompt": "motion blur, distorted faces, abnormal eyes"
         },
-        {
-            "models": {
-                "attn": "videos/9sec/attn_newtest/step-2400",
-            },
-            "step": 2400,
-            "video_length": 9,
-            "neg_prompt": "motion blur, distorted faces, abnormal eyes"
-        },
+        # {
+        #     "models": {
+        #         "attn": "videos/9sec/attn_test/step-2100",
+        #         "m1": "videos/9sec/m1_test/step-2100",
+        #     },
+        #     "step": 2100,
+        #     "video_length": 9,
+        #     "neg_prompt": "motion blur, distorted faces, abnormal eyes"
+        # },
+        # {
+        #     "models": {
+        #         "attn": "videos/9sec/attn_test/step-2400",
+        #     },
+        #     "step": 2400,
+        #     "video_length": 9,
+        #     "neg_prompt": "motion blur, distorted faces, abnormal eyes"
+        # },
         {
             "models": {
                 "mamba": "videos/3sec/mamba2_newtest/step-8000",
                 "attn": "videos/3sec/attn_newtest/step-8000",
                 "m1": "videos/3sec/m1_newtest/step-8000",
-                # "m2": "videos/3sec/m2_newtest/step-8000"
+                "m2": "videos/3sec/m2_newtest/step-8000"
             },
             "step": 8000,
             "video_length": 3,
